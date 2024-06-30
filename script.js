b1177aea38313dd13202932d358263f81a80c595
adamsjs23 <adamaguebre@yahoo.fr>
2024-06-30 20:02:09 +0200

    Modified script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const scroller = document.querySelector(".scroller");
+const scroller2 = document.querySelector(".scroller-2");
+const scroller = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion:reduce)").matches) {
   addAnimation();
 }
 
 function addAnimation() {
-  scroller.setAttribute("data-animated", true);
+  scroller2.setAttribute("data-animated", true);
+  scroller.forEach((scroll) => {
+    scroll.setAttribute("data-animated", true);
+  });
 }
